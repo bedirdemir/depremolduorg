f943b2e189becd4ed56700c92dddd8e51f4de0d0
Bedir Demir <bedir@proton.me>
2023-02-26 01:54:16 +0300

    Add earthquake list

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,3 +1,13 @@
 <template>
-    
+    <earthquakeList></earthquakeList>
 </template>
+
+<script>
+import earthquakeList from "../components/appEarthquakeList/index.vue";
+
+export default {
+    components: {
+        earthquakeList
+    }
+}
+</script>
