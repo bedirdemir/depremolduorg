cca2c4a7753866dc6cd2547f3485e9b61b5616af
Bedir Demir <bedir@proton.me>
2023-02-27 18:46:45 +0300

    Fix font size

--- src/components/appEarthquakeList/index.vue
+++ src/components/appEarthquakeList/index.vue
@@ -14,7 +14,7 @@
                         <div class="flex flex-col w-full">
                             <div>
                                 <h2 class="text-l font-semibold">{{ earthquake.region }}</h2>
-                                <div class="flex space-x-1 items-center">
+                                <div class="flex space-x-1 items-center text-sm lg:text-base">
                                     <svg class="w-3 inline-block text-gray-500" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M12,6a1,1,0,0,0-1,1v5a1,1,0,0,0,.293.707l3,3a1,1,0,0,0,1.414-1.414L13,11.586V7A1,1,0,0,0,12,6Z
                                         M23.812,10.132A12,12,0,0,0,3.578,3.415V1a1,1,0,0,0-2,0V5a2,2,0,0,0,2,2h4a1,1,0,0,0,0-2H4.827a9.99,9.99,0,1,1-2.835,7.878A.982.982,0,0,0,1,12a1.007,1.007,0,0,0-1,1.1,12,12,0,1,0,23.808-2.969Z"/></svg>
