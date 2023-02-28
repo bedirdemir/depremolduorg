9388d1e765b4669a4b2866811fb23c86825196f4
Bedir Demir <bedir@proton.me>
2023-02-28 10:11:06 +0300

    Fix header responsive

--- src/components/Shared/appHeader.vue
+++ src/components/Shared/appHeader.vue
@@ -1,7 +1,7 @@
 <template>
-    <header class="flex justify-between items-center py-5 px-2 border-b border-gray-300 lg:px-1">
+    <header class="flex justify-between items-center py-5 px-3 border-b border-gray-300 lg:px-1">
         <div class="flex items-center space-x-1">
-            <svg class="w-12 text-primary" fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 47 47" xml:space="preserve">
+            <svg class="w-10 lg:w-12 text-primary" fill="currentColor" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 47 47" xml:space="preserve">
         <g>
             <g>
                 <path d="M24.104,41.577c-0.025,0-0.053-0.001-0.078-0.001c-1.093-0.035-2.025-0.802-2.271-1.867l-5.46-23.659l-3.199,8.316
@@ -13,11 +13,11 @@
             </g>
         </g>
             </svg>
-            <h1 class="text-secondary text-xl">depremoldu.org</h1>
+            <h1 class="text-secondary text-lg lg:text-xl">depremoldu.org</h1>
         </div>
         <div>
-            <a class="flex items-center rounded bg-secondary text-white p-2 cursor-pointer" @click="$store.dispatch('getData')">
-                <svg class="w-5 mr-1" fill="currentColor" version="1.1" viewBox="0 0 32 32"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M28,16c-1.219,0-1.797,0.859-2,1.766C25.269,21.03,22.167,26,16,26c-5.523,0-10-4.478-10-10S10.477,6,16,6  c2.24,0,4.295,0.753,5.96,2H20c-1.104,0-2,0.896-2,2s0.896,2,2,2h6c1.104,0,2-0.896,2-2V4c0-1.104-0.896-2-2-2s-2,0.896-2,2v0.518  C21.733,2.932,18.977,2,16,2C8.268,2,2,8.268,2,16s6.268,14,14,14c9.979,0,14-9.5,14-11.875C30,16.672,28.938,16,28,16z"/></svg>
+            <a class="flex items-center rounded text-xs lg:text-base bg-secondary text-white p-2 cursor-pointer" @click="$store.dispatch('getData')">
+                <svg class="w-4 lg:w-5 mr-1" fill="currentColor" version="1.1" viewBox="0 0 32 32"  xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M28,16c-1.219,0-1.797,0.859-2,1.766C25.269,21.03,22.167,26,16,26c-5.523,0-10-4.478-10-10S10.477,6,16,6  c2.24,0,4.295,0.753,5.96,2H20c-1.104,0-2,0.896-2,2s0.896,2,2,2h6c1.104,0,2-0.896,2-2V4c0-1.104-0.896-2-2-2s-2,0.896-2,2v0.518  C21.733,2.932,18.977,2,16,2C8.268,2,2,8.268,2,16s6.268,14,14,14c9.979,0,14-9.5,14-11.875C30,16.672,28.938,16,28,16z"/></svg>
                 Yenile
             </a>
         </div>
