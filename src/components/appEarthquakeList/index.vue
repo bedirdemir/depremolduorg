bfe3530860f7d534722d5c11283c857b4b3b15b4
Bedir Demir <bedir@proton.me>
2023-03-06 04:01:35 +0300

    Fix: eartquake scale table

--- src/components/appEarthquakeList/index.vue
+++ src/components/appEarthquakeList/index.vue
@@ -2,11 +2,11 @@
     <section class="py-3 px-2 min-h-screen lg:px-0">
         <list-item v-if="$store.state.EarthquakeList.isModalActive"></list-item>
         <ul v-if="$store.state.EarthquakeList.isLoaded">
-            <div class="flex items-center justify-center mb-3">
-                <div class="px-2 py-1 bg-yellow-300 text-xs rounded-l">Küçük</div>
-                <div class="px-2 py-1 bg-red-500 text-xs text-white">Orta</div>
-                <div class="px-2 py-1 bg-red-900 text-xs text-white">Büyük</div>
-                <div class="px-2 py-1 bg-zinc-800 text-xs text-white rounded-r">Çok Büyük</div>
+            <div class="flex mb-3 text-center w-full shadow-sm">
+                <div class="px-2 py-1 bg-yellow-300 text-xs  rounded-l w-1/4">Küçük</div>
+                <div class="px-2 py-1 bg-red-500 text-xs text-white w-1/4">Orta</div>
+                <div class="px-2 py-1 bg-red-900 text-xs text-white w-1/4">Büyük</div>
+                <div class="px-2 py-1 bg-zinc-800 text-xs text-white rounded-r w-1/4">Çok Büyük</div>
             </div>
             <li v-for="(earthquake,i) in $store.state.EarthquakeList.list" :key="i" class="mb-2 shadow bg-[#FCFFE760]">
                 <div class="flex justify-between p-3 border rounded w-full">
