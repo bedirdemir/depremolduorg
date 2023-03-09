d73820f1a99790d8c747af69ec0d7de3db40e10a
Bedir Demir <bedir@proton.me>
2023-03-09 16:18:27 +0300

    Update: inform page

--- src/components/information/index.vue
+++ src/components/information/index.vue
@@ -123,6 +123,26 @@
                 </a>
             </div>
 
+            <div class="w-full p-6 border border-gray-200 rounded-lg shadow bg-[#FCFFE760]">
+                <a href="https://www.afad.gov.tr/kurumlar/afad.gov.tr/39499/xfiles/deprem_haritasi.pdf" target="_blank">
+                    <h2 class="mb-3 text-xl lg:text-2xl font-bold tracking-tight text-primary">Türkiye Deprem Tehlike Haritası (AFAD)</h2>
+                </a>
+                <a href="https://www.afad.gov.tr/kurumlar/afad.gov.tr/39499/xfiles/deprem_haritasi.pdf" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-secondary rounded-lg hover:opacity-80">
+                    Görüntüle
+                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+            </div>
+
+            <div class="w-full p-6 border border-gray-200 rounded-lg shadow bg-[#FCFFE760]">
+                <a href="https://www.mta.gov.tr/en/sayfalar/maps/activefault/img/active_fault_1250000.jpg" target="_blank">
+                    <h2 class="mb-3 text-xl lg:text-2xl font-bold tracking-tight text-primary">Türkiye Diri Fay Haritası (MTA)</h2>
+                </a>
+                <a href="https://www.mta.gov.tr/en/sayfalar/maps/activefault/img/active_fault_1250000.jpg" target="_blank" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-secondary rounded-lg hover:opacity-80">
+                    Görüntüle
+                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </a>
+            </div>
+
         </div>
 
     </section>
